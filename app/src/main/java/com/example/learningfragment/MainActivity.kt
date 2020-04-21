b0e1cbd24084e868453e6a137e13fa728c30301b
Gabriel Neri <neri.gabriel78@gmail.com>
2020-04-21 16:56:14 -0300

    quiz game with android and fragment

---
+++ app/src/main/java/com/example/learningfragment/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.learningfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.learningfragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+}
